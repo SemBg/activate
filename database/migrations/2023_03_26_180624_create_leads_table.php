2401ec54aba9443b595c9f2b50363fd3e28d4236
Sem <sembloemberg@gmail.com>
2023-03-26 20:53:38 +0200

    Leads migration

---
+++ database/migrations/2023_03_26_180624_create_leads_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('leads', function (Blueprint $table) {
+            $table->id();
+            $table->string('voornaam');
+            $table->string('achternaam');
+            $table->string('telefoonnummer');
+            $table->string('email');
+            $table->string('omschrijving');
+            $table->string('bijlage')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('leads');
+    }
+};
