10087606ba77d454087e73145201dd8d8c8986d8
Sem <sembloemberg@gmail.com>
2023-03-27 22:23:17 +0200

    added a homepage

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<x-layout>
+	<div class="flex h-screen">
+		<div class="m-auto">
+			<a href="/login" class="h-12 w-24 bg-red-500">Login</a>
+			<a href="/register" class="h-12 w-24 bg-red-500">Register</a>
+			<a href="/leads" class="h-12 w-24 bg-red-500">Leads</a>
+			<a href="/create-lead" class="h-12 w-24 bg-red-500">Create lead</a>
+		</div>
+	</div>
+</x-layout>
