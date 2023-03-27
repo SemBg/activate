bbeb72e07e02117efc4a1a432b5238277c584b1e
Sem <sembloemberg@gmail.com>
2023-03-27 22:13:38 +0200

    added storeapi() function

--- app/Http/Controllers/LeadController.php
+++ app/Http/Controllers/LeadController.php
@@ -34,6 +34,27 @@ class LeadController extends Controller
     return redirect('/');
   }
 
+  public function storeapi(Request $request) {
+    //* Valideer of alle velden correct zijn ingevuld
+    $formFields = $request->validate([
+      'voornaam' => 'required',
+      'achternaam' => 'required',
+      'telefoonnummer' => ['required', 'numeric', 'min:10'],
+      'email' => ['required', 'email'],
+      'omschrijving' => 'required',
+      'toestemming' => 'required'
+    ]);
+
+    //* Check of de request een bijlage heeft
+    if ($request->hasFile('bijlage')) {
+      //* Sla bijlage op in /storage/app/public/bijlages, sla path op in db
+      $formFields['bijlage'] = $request->file('bijlage')->store('bijlages', 'public');
+    }
+
+    Lead::create($formFields);
+    return 'Lead succesfully created!';
+  }
+
   public function index() {
     return view('leads.index', [
       'leads' => Lead::all()
@@ -44,5 +65,9 @@ class LeadController extends Controller
     return view('leads.show', [
       'lead' => $lead
     ]);
+  }
+
+  public function getlead(Lead $lead) {
+    return $lead;
   }
 }
